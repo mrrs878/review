a084b1eb234f17b7f7646f07c5b503ca2fb46c69
mrrs878 <mrrs878@foxmail.com>
2021-05-13 21:33:27 +0800

    feat: 添加iterator

---
+++ src/js/iterator.js
@@ -0,0 +1,26 @@
+/*
+ * @Author: your name
+ * @Date: 2021-05-12 22:32:30
+ * @LastEditTime: 2021-05-12 22:44:26
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \review\src\js\iterator.js
+ */
+const iterable = {
+  [Symbol.iterator]: () => {
+    let count = 0;
+    return {
+      next() {
+        count += 1;
+        if (count <= 2) return { done: false, value: count };
+        return { done: true, value: undefined };
+      },
+      return() {
+        console.log('exiting early');
+        return { done: true, value: undefined };
+      },
+    };
+  },
+};
+
+export default iterable;
---
+++ src/js/iterator.test.js
@@ -0,0 +1,44 @@
+/*
+ * @Author: your name
+ * @Date: 2021-05-12 22:02:45
+ * @LastEditTime: 2021-05-12 22:48:08
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \review\src\js\iterator.test.js
+ */
+import iterable from './iterator';
+
+test('iterator protocol', () => {
+  const arr = ['tom', 'jerry'];
+
+  const iter1 = arr[Symbol.iterator]();
+  expect(iter1.next().value).toBe('tom');
+  expect(iter1.next().value).toBe('jerry');
+  expect(iter1.next().done).toBeTruthy();
+
+  const iter2 = arr[Symbol.iterator]();
+  expect(iter2.next().done).toBeFalsy();
+
+  const iter3 = arr[Symbol.iterator]();
+  expect(iter3.next().value).toBe('tom');
+  arr.splice(1, 0, 'jack');
+  expect(iter3.next().value).toBe('jack');
+  arr[2] = 'tiny jerry';
+  expect(iter3.next().value).toBe('tiny jerry');
+});
+
+test('custom iterable', () => {
+  const iter = iterable[Symbol.iterator]();
+
+  expect(iter.next().value).toBe(1);
+  expect(iter.next().value).toBe(2);
+  expect(iter.next().done).toBeTruthy();
+});
+
+test('exiting early', () => {
+  console.log = jest.fn();
+  const [a, b] = iterable;
+  expect(a).toBe(1);
+  expect(b).toBe(2);
+  expect(console.log).toHaveBeenCalledWith('exiting early');
+});
