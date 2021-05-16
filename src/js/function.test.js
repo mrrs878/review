2f7faf4d7cb3fec9ba0cd043384a2592604912bf
mrrs878 <mrrs878@foxmail.com>
2021-05-16 10:19:16 +0800

    test: 添加new.target测试用例

--- src/js/function.test.js
+++ src/js/function.test.js
@@ -1,14 +1,14 @@
 /*
  * @Author: mrrs878@foxmail.com
  * @Date: 2021-05-15 22:38:24
- * @LastEditTime: 2021-05-15 22:45:06
+ * @LastEditTime: 2021-05-16 10:18:12
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
- * @FilePath: \review\src\js\function_name.test.js
+ * @FilePath: \review\src\js\function.test.js
  */
 import {
   fn1, fn2, fn3, fn4, obj,
-} from './function_name';
+} from './function';
 
 describe('function[name]', () => {
   test('normal function', () => {
@@ -29,5 +29,29 @@ describe('function[name]', () => {
 
   test('(bind function)[name]', () => {
     expect(fn3.name).toEqual('bound fn1');
+  });
+
+  test('Function.new.target', () => {
+    const errorMessage = 'User muse be instantiated using "new"';
+    const successMessage = 'User instantiated using "new"';
+
+    console.log = jest.fn();
+
+    function User() {
+      if (!new.target) {
+        throw new Error(errorMessage);
+      }
+      console.log(successMessage);
+    }
+
+    try {
+      User();
+    } catch (e) {
+      expect(e.message).toEqual(errorMessage);
+    }
+
+    // eslint-disable-next-line no-unused-vars
+    const user = new User();
+    expect(console.log).toBeCalledWith(successMessage);
   });
 });
