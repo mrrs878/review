80c6589c998b417bcd42a0f93ffa3dd1d7c23907
mrrs878 <mrrs878@foxmail.com>
2021-06-06 21:55:38 +0800

    test: 添加list相关算法题

--- src/algorithm/list.js
+++ src/algorithm/list.js
@@ -2,7 +2,7 @@
  * @Date: 2021-06-05 23:16:17
  * @Author: mrrs878@foxmail.com
  * @LastEditors: mrrs878@foxmail.com
- * @LastEditTime: 2021-06-06 00:04:24
+ * @LastEditTime: 2021-06-06 21:51:40
  * @FilePath: \review\src\algorithm\list.js
  */
 class ListNode {
@@ -20,7 +20,9 @@ function list2array(list) {
   }
   return array;
 }
+
 function array2list(array) {
+  if (array.length === 0) return null;
   const list = new ListNode(array[0], null);
   let pointer = list;
   for (let i = 1; i < array.length; i += 1) {
@@ -29,6 +31,18 @@ function array2list(array) {
   }
   return list;
 }
+
+function skipNNodes(head, n) {
+  let m = n;
+  let pointer = head;
+  while (m > 0 && pointer) {
+    m -= 1;
+    pointer = pointer.next;
+  }
+
+  return pointer;
+}
+
 function mergeTwoLists(l1, l2) {
   const arr1 = list2array(l1);
   const arr2 = list2array(l2);
@@ -37,4 +51,73 @@ function mergeTwoLists(l1, l2) {
   return array2list(res);
 }
 
-export { ListNode, mergeTwoLists, list2array };
+function hasCycle(list) {
+  const nodeMap = new Map();
+  let pointer = list;
+  let pos = 0;
+  if (pointer === null) return false;
+  if (pointer.next === null) return false;
+  while (pointer) {
+    if (nodeMap.has(pointer) && nodeMap.get(pointer) < pos) return true;
+    nodeMap.set(pointer, pos);
+    pointer = pointer.next;
+    pos += 1;
+  }
+  return false;
+}
+
+function reverseList(list) {
+  const arr = list2array(list);
+  const reverseArr = arr.reverse();
+  const res = array2list(reverseArr);
+  return res;
+}
+
+function getMiddle(list) {
+  let pointer2 = list;
+  let pointer1 = list;
+
+  while (pointer2.next !== null && pointer2.next.next !== null) {
+    pointer1 = pointer1.next;
+    pointer2 = pointer2.next.next;
+  }
+
+  return pointer2.next && pointer2.next.next === null ? pointer1.next : pointer1;
+}
+
+function removeNthFromEnd(head, n) {
+  const arr = [];
+  let pointer = head;
+  while (pointer) {
+    arr.push(pointer);
+    pointer = pointer.next;
+  }
+  const index = arr.length - n;
+  if (index === 0) {
+    arr[0].next = null;
+    return arr[1] || [];
+  }
+  arr[index - 1].next = arr[index].next;
+  arr[index].next = null;
+
+  return head;
+}
+
+function removeNthFromEndPreform(head, n) {
+  const preHead = new ListNode(0);
+  preHead.next = head;
+  let pointer1 = preHead;
+  let pointer2 = skipNNodes(head, n - 1);
+
+  while (pointer2 && pointer2.next) {
+    pointer2 = pointer2.next;
+    pointer1 = pointer1.next;
+  }
+  pointer1.next = pointer1.next.next;
+  return preHead.next;
+}
+
+export {
+  ListNode, mergeTwoLists, list2array, hasCycle, array2list, reverseList,
+  getMiddle, removeNthFromEnd, removeNthFromEndPreform,
+};
--- src/algorithm/list.test.js
+++ src/algorithm/list.test.js
@@ -2,13 +2,17 @@
  * @Date: 2021-06-05 23:24:02
  * @Author: mrrs878@foxmail.com
  * @LastEditors: mrrs878@foxmail.com
- * @LastEditTime: 2021-06-06 00:03:18
+ * @LastEditTime: 2021-06-06 21:44:14
  * @FilePath: \review\src\algorithm\list.test.js
  */
-import { list2array, ListNode, mergeTwoLists } from './list';
+import {
+  hasCycle, list2array, ListNode, mergeTwoLists, array2list, reverseList, getMiddle,
+  removeNthFromEnd,
+  removeNthFromEndPreform,
+} from './list';
 
 describe('list problems', () => {
-  test('mergeTwoLists', () => {
+  test('mergeTwoLists case1', () => {
     const list1 = new ListNode(1,
       new ListNode(2,
         new ListNode(4, null)));
@@ -25,5 +29,110 @@ describe('list problems', () => {
                 new ListNode(7, null)))))));
     const tmp = mergeTwoLists(list1, list2);
     expect(list2array(tmp)).toEqual(list2array(res));
+  });
+
+  test('mergeTwoLists case2', () => {
+    const list1 = null;
+    const list2 = new ListNode(0, null);
+    const res = new ListNode(0, null);
+    const tmp = mergeTwoLists(list1, list2);
+    expect(list2array(tmp)).toEqual(list2array(res));
+  });
+  test('mergeTwoLists case3', () => {
+    const list1 = null;
+    const list2 = null;
+    const res = null;
+    const tmp = mergeTwoLists(list1, list2);
+    expect(list2array(tmp)).toEqual(list2array(res));
+  });
+
+  test('hasCycle case1', () => {
+    const node1 = new ListNode(1, null);
+    const node2 = new ListNode(2, null);
+    const node3 = new ListNode(3, null);
+    const node4 = new ListNode(4, null);
+
+    expect(hasCycle(null)).toEqual(false);
+    expect(hasCycle(node1)).toEqual(false);
+
+    node1.next = node2;
+    node2.next = node3;
+    node3.next = node4;
+    expect(hasCycle(node1)).toEqual(false);
+
+    node4.next = node2;
+    expect(hasCycle(node1)).toEqual(true);
+
+    node3.next = node3;
+    expect(hasCycle(node1)).toEqual(true);
+  });
+
+  test('hasCycle case2', () => {
+    const list = array2list(
+      [-21, 10, 17, 8, 4, 26, 5, 35, 33, -7, -16, 27, -12, 6,
+        29, -12, 5, 9, 20, 14, 14, 2, 13, -24, 21, 23, -21, 5],
+    );
+    expect(hasCycle(list)).toEqual(false);
+  });
+
+  test('reverseList case1', () => {
+    const arr = [1, 2, 3, 4, 5];
+    const list = array2list(arr);
+    const res = reverseList(list);
+    expect(list2array(res)).toEqual(arr.reverse());
+  });
+
+  test('getMiddle case1', () => {
+    const arr = [1, 2, 3, 4, 5];
+    const list = array2list(arr);
+    const res = getMiddle(list);
+    expect(list2array(res)).toEqual([3, 4, 5]);
+  });
+  test('getMiddle case2', () => {
+    const arr = [1, 2, 3, 4, 5, 6];
+    const list = array2list(arr);
+    const res = getMiddle(list);
+    expect(list2array(res)).toEqual([4, 5, 6]);
+  });
+
+  test('removeNthFromEnd case1', () => {
+    const arr = [1, 2, 3, 4, 5, 6];
+    const list = array2list(arr);
+    const res1 = removeNthFromEnd(list, 3);
+    expect(list2array(res1)).toEqual([1, 2, 3, 5, 6]);
+
+    const res2 = removeNthFromEnd(list, 1);
+    expect(list2array(res2)).toEqual([1, 2, 3, 5]);
+
+    const res3 = removeNthFromEnd(list, 4);
+    expect(list2array(res3)).toEqual([2, 3, 5]);
+  });
+  test('removeNthFromEnd case2', () => {
+    const arr = [1, 2, 3, 4, 5, 6, 4, 5, 1, 9, 4, 1];
+    const res1 = removeNthFromEnd(array2list(arr), 3);
+    arr.splice(arr.length - 3, 1);
+    expect(list2array(res1)).toEqual(arr);
+
+    const res2 = removeNthFromEnd(array2list(arr), 1);
+    arr.splice(arr.length - 1, 1);
+    expect(list2array(res2)).toEqual(arr);
+
+    const res3 = removeNthFromEnd(array2list(arr), arr.length);
+    arr.splice(arr.length - arr.length, 1);
+    expect(list2array(res3)).toEqual(arr);
+  });
+  test('removeNthFromEndPerform case1', () => {
+    const arr = [1, 2, 3, 4, 5, 6, 4, 5, 1, 9, 4, 1];
+    const res1 = removeNthFromEndPreform(array2list(arr), 3);
+    arr.splice(arr.length - 3, 1);
+    expect(list2array(res1)).toEqual(arr);
+
+    const res2 = removeNthFromEndPreform(array2list(arr), 1);
+    arr.splice(arr.length - 1, 1);
+    expect(list2array(res2)).toEqual(arr);
+
+    const res3 = removeNthFromEndPreform(array2list(arr), arr.length);
+    arr.splice(arr.length - arr.length, 1);
+    expect(list2array(res3)).toEqual(arr);
   });
 });
