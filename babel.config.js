f1c9346155f9a243e30806ebb7aa7143db8f1b03
mrrs878 <mrrs878@foxmail.com>
2021-06-12 18:47:01 +0800

    ci: test脚本添加coverage配置

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,9 @@
 /*
- * @Date: 2021-06-08 13:31:46
- * @Author: mrrs878@foxmail.com
- * @LastEditors: mrrs878@foxmail.com
- * @LastEditTime: 2021-06-08 13:33:36
- * @FilePath: /review/babel.config.js
- */
-/*
  * @Date: 2021-06-08 13:28:33
  * @Author: mrrs878@foxmail.com
  * @LastEditors: mrrs878@foxmail.com
- * @LastEditTime: 2021-06-08 13:29:35
- * @FilePath: /review/babel.config.js
+ * @LastEditTime: 2021-06-12 18:37:21
+ * @FilePath: \review\babel.config.js
  */
 module.exports = function (api) {
   api.cache(true);
