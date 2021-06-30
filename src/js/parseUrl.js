1de35e13984643025dc312b198ff01043f79cbe1
mrrs878 <mrrs878@foxmail.com>
2021-06-30 23:41:07 +0800

    test: 完善pathURL及其测试用例

--- src/js/parseUrl.js
+++ src/js/parseUrl.js
@@ -2,9 +2,36 @@
  * @Date: 2021-06-30 17:01:10
  * @Author: mrrs878@foxmail.com
  * @LastEditors: mrrs878@foxmail.com
- * @LastEditTime: 2021-06-30 18:46:07
- * @FilePath: /review/src/js/parseUrl.js
+ * @LastEditTime: 2021-06-30 23:39:52
+ * @FilePath: \review\src\js\parseUrl.js
  */
+
+/**
+ * @description:
+ * @param {Object} obj
+ * @param {string} path
+ * @return {*}
+ */
+const getValue = (obj, path) => path.reduce((pre, cur) => {
+  // eslint-disable-next-line no-param-reassign
+  if (!pre[cur]) pre[cur] = {};
+  return pre[cur];
+}, obj);
+
+/**
+ * @description:
+ * @param {Object} obj
+ * @param {Array<string>} path
+ * @param {*} value
+ * @return {*}
+ */
+function setProp(obj, path, value) {
+  const { length } = path;
+  const prop = path.slice(length - 1);
+
+  const target = getValue(obj, path.slice(0, length - 1)) || {};
+  target[prop] = value;
+}
 
 /**
  * @description:
@@ -14,26 +41,27 @@
 function parseUrl(url) {
   if (url === '') return {};
 
-  const queryString = url.match(/\?([^/?#:]+)#?/)?.[1];
+  const [, queryString] = url.match(/\?([^/?#:]+)#?/) || [];
   if (!queryString) return {};
 
-  const isNumber = (v) => v.match(/^\d+$/);
-  const isObject = (v) => v.match(/(\[\w+\])|(\w.\w)$/g);
-  const shakePath = (v) => v.replace(/\[\]$/g, '');
-  const formatPath = (v) => v.split('.').reduce((pre, cur) => `${pre}[${cur}]`);
+  const formatKeyIfArray = (v) => v.replace(/\[\]$/g, '');
+  const formatKeyIfObject = (v) => v.replace(/\[/, '.').replace(/\]/, '');
+  const formatValueIfNumber = (v = '') => (v.match(/^\d+$/) ? parseInt(v, 10) : v);
 
   const res = queryString.split('&').reduce((params, block) => {
     const tmp = JSON.parse(JSON.stringify(params));
     const [k, v] = block.split('=');
 
     let key = decodeURIComponent(k);
-    let value = decodeURIComponent(v);
+    let value = v ? decodeURIComponent(v) : undefined;
 
-    key = shakePath(key);
-    key = isObject(key) ? formatPath(key) : key;
-    value = isNumber(value) ? parseInt(value, 10) : value;
+    key = formatKeyIfArray(key);
+    key = formatKeyIfObject(key);
+    value = formatValueIfNumber(value);
 
-    tmp[key] = tmp[key] ? [].concat(tmp[key], value) : value;
+    if (tmp[key]) {
+      tmp[key] = [].concat(tmp[key], value);
+    } else setProp(tmp, key.split('.'), value);
 
     return tmp;
   }, {});
--- src/js/parseUrl.test.js
+++ src/js/parseUrl.test.js
@@ -2,15 +2,19 @@
  * @Date: 2021-06-30 17:38:33
  * @Author: mrrs878@foxmail.com
  * @LastEditors: mrrs878@foxmail.com
- * @LastEditTime: 2021-06-30 19:09:06
- * @FilePath: /review/src/js/parseUrl.test.js
+ * @LastEditTime: 2021-06-30 23:39:29
+ * @FilePath: \review\src\js\parseUrl.test.js
  */
 import { parseUrl } from './parseUrl';
 
 describe('parse url', () => {
   test('simple', () => {
-    const url = 'https://xxx.com?name=tom';
-    expect(parseUrl(url)).toEqual({ name: 'tom' });
+    const url = 'https://xxx.com?name=tom&age=21';
+    expect(parseUrl(url)).toEqual({ name: 'tom', age: 21 });
+  });
+
+  test('empty', () => {
+    expect(parseUrl('https://xxx.com')).toEqual({});
   });
 
   test('array', () => {
@@ -18,8 +22,13 @@ describe('parse url', () => {
     expect(parseUrl(url)).toEqual({ a: [1, 2] });
   });
 
-  test.skip('object', () => {
-    const url = 'https://xxx.com?a[b]=1&a[c]=2';
+  test('object', () => {
+    const url = 'https://xxx.com?a[b]=1&a.c=2';
     expect(parseUrl(url)).toEqual({ a: { b: 1, c: 2 } });
+  });
+
+  test('complex', () => {
+    const url = 'https://xxx.com?a[b]=1&a.c=2&a.e.d=3&c=1&c[]=2&d#name';
+    expect(parseUrl(url)).toEqual({ a: { b: 1, c: 2, e: { d: 3 } }, c: [1, 2], d: '' });
   });
 });
