f31fe79e5284cb0ddaa5ed1d291251e144f39114
mrrs878 <mrrs878@foxmail.com>
2021-07-01 23:15:46 +0800

    feat: 添加另一种flatten实现

--- src/js/flat.js
+++ src/js/flat.js
@@ -2,7 +2,7 @@
  * @Date: 2021-06-03 22:01:26
  * @Author: mrrs878@foxmail.com
  * @LastEditors: mrrs878@foxmail.com
- * @LastEditTime: 2021-06-03 22:21:51
+ * @LastEditTime: 2021-07-01 23:14:09
  * @FilePath: \review\src\js\flat.js
  */
 function flat(array, depth = 1) {
@@ -23,4 +23,8 @@ function flat(array, depth = 1) {
   return res;
 }
 
-export default flat;
+const flatten = (arr, dep = 1) => (dep > 0
+  ? arr.reduce((pre, cur) => pre.concat(Array.isArray(cur) ? flatten(cur, dep - 1) : cur), [])
+  : arr.slice());
+
+export { flat, flatten };
--- src/js/flat.test.js
+++ src/js/flat.test.js
@@ -2,24 +2,27 @@
  * @Date: 2021-06-03 22:08:31
  * @Author: mrrs878@foxmail.com
  * @LastEditors: mrrs878@foxmail.com
- * @LastEditTime: 2021-06-06 21:55:10
+ * @LastEditTime: 2021-07-01 23:14:30
  * @FilePath: \review\src\js\flat.test.js
  */
-import flat from './flat';
+import { flat, flatten } from './flat';
 
 describe('Array.flat', () => {
   test('default', () => {
     const target = [1, [2, 3], 4];
     expect(flat(target)).toEqual([1, 2, 3, 4]);
+    expect(flatten(target)).toEqual([1, 2, 3, 4]);
   });
 
   test('depth @ 2', () => {
     const target = [1, [2, 3], 4, [5, [6, 7]]];
     expect(flat(target, 2)).toEqual([1, 2, 3, 4, 5, 6, 7]);
+    expect(flatten(target, 2)).toEqual([1, 2, 3, 4, 5, 6, 7]);
   });
 
   test('depth @ 1', () => {
     const target = [1, [2, 3], 4, [5, [6, 7]]];
     expect(flat(target)).toEqual([1, 2, 3, 4, 5, [6, 7]]);
+    expect(flatten(target)).toEqual([1, 2, 3, 4, 5, [6, 7]]);
   });
 });
