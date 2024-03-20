85be63747978fee103855d33c41d27ede218fdae
mrrs878 <mrrs878@foxmail.com>
2024-03-20 20:40:26 +0800

    chore: 添加react合成事件

--- src/react/eventSystem/index.jsx
+++ src/react/eventSystem/index.jsx
@@ -2,13 +2,13 @@
  * @Date: 2021-06-02 15:38:28
  * @Author: mrrs878@foxmail.com
  * @LastEditors: mrrs878@foxmail.com
- * @LastEditTime: 2021-06-02 16:19:49
+ * @LastEditTime: 2024-03-20 20:34:50
  * @FilePath: /review/src/react/eventSystem/index.jsx
  */
 /* eslint-disable react/prop-types */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
@@ -21,26 +21,54 @@ function Modal({ title }) {
 }
 
 function App() {
-  const [isShowModal, setIsShowModal] = useState(false);
+  /** @type{React.MutableRefObject<HTMLDivElement>} */
+  const mainRef = React.useRef();
+  const childRef = React.useRef();
+
+  useEffect(() => {
+    const onMainClick = () => {
+      console.log('main DOM');
+    };
+
+    const onChildClick = () => {
+      console.log('child DOM');
+    };
+
+    const mainDOM = mainRef.current;
+    const childDOM = childRef.current;
+
+    mainDOM.addEventListener('click', onMainClick);
+    childDOM.addEventListener('click', onChildClick);
+
+    return () => {
+      mainDOM.removeEventListener('click', onMainClick);
+      childDOM.removeEventListener('click', onChildClick);
+    };
+  }, []);
+
   return (
     <div
       className="app"
       role="main"
+      ref={mainRef}
       onClick={() => {
-        setIsShowModal(false);
-        console.log(111);
+        console.log('main');
+      }}
+      onClickCapture={() => {
+        console.log('main Capture');
       }}
     >
-      {isShowModal && <Modal title="hello" />}
-      <button
-        type="button"
-        onClick={(e) => {
-          setIsShowModal(true);
-          e.stopPropagation();
+      <div
+        role="main"
+        className="child"
+        ref={childRef}
+        onClick={() => {
+          console.log('child');
         }}
-      >
-        show modal
-      </button>
+        onClickCapture={() => {
+          console.log('child Capture');
+        }}
+      />
     </div>
   );
 }
